4006492a3ae12cc34fdc0970e94ad9fe2d7da6f8
Ami Fukuyama <amifuku80@gmail.com>
2025-12-12 10:46:56 +0800

    Added log-icon

--- app/log-page.tsx
+++ app/log-page.tsx
@@ -1,12 +1,18 @@
 import { useRouter } from "expo-router";
-import { Text, TouchableOpacity, View, useColorScheme } from "react-native";
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  useColorScheme,
+} from "react-native";
 import { logOut } from "../src/auth/authMethods";
 
 export default function LogPage() {
   const router = useRouter();
   const colorScheme = useColorScheme();
 
-  const bgColor = colorScheme === "dark" ? "bg-gray-900" : "bg-white";
+  const bgColor = colorScheme === "dark" ? "bg-gray-900" : "bg-[#FFF6E8]";
   const textColor = colorScheme === "dark" ? "text-white" : "text-black";
 
   return (
@@ -16,16 +22,26 @@ export default function LogPage() {
       </Text>
 
       <TouchableOpacity
-        className="bg-orange-400 rounded-full w-20 h-20 justify-center items-center mb-8"
+        className="bg-[#DCE8D2] rounded-full w-50 h-50 justify-center items-center mb-8"
+        style={{
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 4 },
+          shadowOpacity: 0.3,
+          shadowRadius: 6,
+          elevation: 8, // for Android
+        }}
         onPress={() => router.push("/diary")}
       >
-        <Text className="text-white text-3xl">📝</Text>
+        <Image
+          source={require("../assets/images/log-icon.png")}
+          style={{ width: 100, height: 100 }}
+        />
       </TouchableOpacity>
 
       <Text className="text-gray-500 mb-8">Tap to write your story</Text>
 
       <TouchableOpacity onPress={logOut}>
-        <Text className="text-orange-400">Logout</Text>
+        <Text className="text-[#F4A373]">Logout</Text>
       </TouchableOpacity>
     </View>
   );
